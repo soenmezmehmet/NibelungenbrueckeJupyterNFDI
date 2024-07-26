7f505046a9101a690da2be770af6c0528c5ab9e1
msoenmez <msoenmez@VDI0070.zit.bam.de>
2024-07-26 14:06:48 +0200

    fastapi wihtout authentication credentials and separation of output files

--- nibelungenbruecke/scripts/digital_twin_orchestrator/orchestrator.py
+++ nibelungenbruecke/scripts/digital_twin_orchestrator/orchestrator.py
@@ -24,12 +24,13 @@ class Orchestrator:
     def compare(self, output, input_value): #TODO
         self.updated = (output == 2 * input_value)
 
-    def run(self):
-        input_value=round(2.7*10**11, 1)   #TODO: !!
+    def run(self, input_value, model_to_run):
+        #input_value=round(2.7*10**11, 1)   #TODO: !!
         model_path = self.orchestrator_parameters["model_path"]
         model_parameters = self.orchestrator_parameters["generation_models_list"][0]["model_parameters"]
         dt_path = self.orchestrator_parameters["generation_models_list"][0]["digital_twin_parameters_path"]
-        digital_twin = DigitalTwin(model_path, model_parameters, dt_path, model_to_run = "Displacement_2")
+        #digital_twin = DigitalTwin(model_path, model_parameters, dt_path, model_to_run = "Displacement_2")
+        digital_twin = DigitalTwin(model_path, model_parameters, dt_path, model_to_run)
         prediction = self.predict_dt(digital_twin, input_value)
         #prediction = self.predict_last_week(digital_twin, input_value)
         print("Prediction:", prediction) #TODO
@@ -41,7 +42,9 @@ if __name__ == "__main__":
     #    a = json.load(file)
     
     orchestrator = Orchestrator(path)
-    orchestrator.run()
+    model_to_run = "Displacement_1"
+    input_value=round(2.7*10**11, 1)
+    orchestrator.run(input_value, model_to_run)
     
     #import os
     #print(os.getcwd())
